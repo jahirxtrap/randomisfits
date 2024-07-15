de15d93a35259a28199b3f2e31662710d3202d6b
jahirxtrap <jahirroman2002@gmail.com>
2024-07-14 21:42:55 -0500

    Fixes

--- neoforge/src/main/java/com/jahirtrap/randomisfits/event/RepairItemEvent.java
+++ neoforge/src/main/java/com/jahirtrap/randomisfits/event/RepairItemEvent.java
@@ -2,21 +2,17 @@ package com.jahirtrap.randomisfits.event;
 
 import com.jahirtrap.randomisfits.item.BaseRepairKitItem;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-
-import static com.jahirtrap.randomisfits.util.CommonUtils.checkCreativeMode;
 
 public class RepairItemEvent {
-    public static boolean execute(Level level, Player player, int amount) {
+    public static boolean execute(Player player, int amount) {
         ItemStack mainHandIst = player.getMainHandItem(), offHandIst = player.getOffhandItem();
 
         if (isRepairKit(mainHandIst) && isRepairable(offHandIst))
-            return repairItem(level, player, offHandIst, mainHandIst, amount);
+            return repairItem(player, offHandIst, mainHandIst, amount);
         else if (isRepairKit(offHandIst) && isRepairable(mainHandIst))
-            return repairItem(level, player, mainHandIst, offHandIst, amount);
+            return repairItem(player, mainHandIst, offHandIst, amount);
 
         return false;
     }
@@ -29,12 +25,11 @@ public class RepairItemEvent {
         return stack.getDamageValue() > 0 && stack.getCount() == 1;
     }
 
-    private static boolean repairItem(Level level, Player player, ItemStack result, ItemStack stack, int amount) {
+    private static boolean repairItem(Player player, ItemStack result, ItemStack stack, int amount) {
         result.setDamageValue(result.getDamageValue() - amount);
-        if (!checkCreativeMode(player)) stack.shrink(1);
-        player.getInventory().setChanged();
+        if (!player.getAbilities().instabuild) stack.shrink(1);
 
-        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ANVIL_USE, SoundSource.PLAYERS, 1, 1);
+        player.playSound(SoundEvents.ANVIL_USE, 1, 1);
         return true;
     }
 }
