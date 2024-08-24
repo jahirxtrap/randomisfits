36f7a8293c0d3a40d758962e2792b0f6f791dec2
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:38:09 -0500

    Update config lib

--- forge/src/main/java/com/jahirtrap/randomisfits/RandomisfitsMod.java
+++ forge/src/main/java/com/jahirtrap/randomisfits/RandomisfitsMod.java
@@ -1,13 +1,11 @@
 package com.jahirtrap.randomisfits;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.randomisfits.init.ModConfig;
 import com.jahirtrap.randomisfits.init.ModContent;
 import com.jahirtrap.randomisfits.init.ModTab;
 import com.jahirtrap.randomisfits.network.PacketHandler;
-import com.jahirtrap.randomisfits.util.configlib.TXFConfig;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -20,9 +18,6 @@ public class RandomisfitsMod {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         TXFConfig.init(MODID, ModConfig.class);
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
-
         ModContent.init(bus);
         ModTab.init(bus);
         PacketHandler.init();
