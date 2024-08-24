54d8c4e0f0d93415b052a450f7b8d9ddf8fda3e4
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:39:52 -0500

    Update config lib

--- neoforge/src/main/java/com/jahirtrap/randomisfits/RandomisfitsMod.java
+++ neoforge/src/main/java/com/jahirtrap/randomisfits/RandomisfitsMod.java
@@ -1,14 +1,12 @@
 package com.jahirtrap.randomisfits;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.randomisfits.init.ModConfig;
 import com.jahirtrap.randomisfits.init.ModContent;
 import com.jahirtrap.randomisfits.init.ModTab;
 import com.jahirtrap.randomisfits.network.PacketHandler;
-import com.jahirtrap.randomisfits.util.configlib.TXFConfig;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(RandomisfitsMod.MODID)
 public class RandomisfitsMod {
@@ -17,9 +15,6 @@ public class RandomisfitsMod {
 
     public RandomisfitsMod(IEventBus bus) {
         TXFConfig.init(MODID, ModConfig.class);
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
-
         ModContent.init(bus);
         ModTab.init(bus);
         PacketHandler.init(bus);
