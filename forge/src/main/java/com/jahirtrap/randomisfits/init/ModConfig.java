630084b4049fb90412c13ae6e2995011120dbc00
jahirxtrap <jahirroman2002@gmail.com>
2024-12-28 03:46:22 -0500

    v1.2.2

--- forge/src/main/java/com/jahirtrap/randomisfits/init/ModConfig.java
+++ forge/src/main/java/com/jahirtrap/randomisfits/init/ModConfig.java
@@ -3,28 +3,28 @@ package com.jahirtrap.randomisfits.init;
 import com.jahirtrap.configlib.TXFConfig;
 
 public class ModConfig extends TXFConfig {
-    @Entry(name = "Multitool Interactions")
+    @Entry(name = "Multitool Interactions", itemDisplay = "randomisfits:iron_multitool")
     public static boolean multitoolInteractions = true;
-    @Entry(name = "Toggle Hammer Mode")
+    @Entry(name = "Toggle Hammer Mode", itemDisplay = "randomisfits:iron_hammer")
     public static boolean toggleHammerMode = true;
-    @Entry(name = "Toggle Excavator Mode")
+    @Entry(name = "Toggle Excavator Mode", itemDisplay = "randomisfits:iron_excavator")
     public static boolean toggleExcavatorMode = true;
-    @Entry(name = "Toggle Lumberaxe Felling")
+    @Entry(name = "Toggle Lumberaxe Felling", itemDisplay = "randomisfits:iron_lumberaxe")
     public static boolean toggleLumberaxeFelling = true;
-    @Entry(name = "Felling Limit", min = 1, max = Integer.MAX_VALUE)
+    @Entry(name = "Felling Limit", min = 1, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:iron_lumberaxe")
     public static int fellingLimit = 512;
-    @Entry(name = "Kit Repair Amount", min = 1, max = Integer.MAX_VALUE)
+    @Entry(name = "Kit Repair Amount", min = 1, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:repair_kit")
     public static int kitRepairAmount = 150;
-    @Entry(name = "Diamond Kit Repair Amount", min = 1, max = Integer.MAX_VALUE)
+    @Entry(name = "Diamond Kit Repair Amount", min = 1, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:diamond_repair_kit")
     public static int diamondKitRepairAmount = 600;
-    @Entry(name = "Netherite Kit Repair Amount", min = 1, max = Integer.MAX_VALUE)
+    @Entry(name = "Netherite Kit Repair Amount", min = 1, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:netherite_repair_kit")
     public static int netheriteKitRepairAmount = 900;
-    @Entry(name = "Wearable Light Blocks")
+    @Entry(name = "Wearable Light Blocks", itemDisplay = "randomisfits:lamp")
     public static boolean wearableLightBlocks = true;
-    @Entry(name = "Right Click Slot Open Menu")
+    @Entry(name = "Right Click Slot Open Menu", itemDisplay = "randomisfits:crafting_plate")
     public static boolean rightClickSlotOpenMenu = true;
-    @Entry(name = "Item Autorepair Interval", min = 0, max = Integer.MAX_VALUE)
+    @Entry(name = "Item Autorepair Interval", min = 0, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:zurite_ingot")
     public static int itemAutorepairInterval = 30;
-    @Entry(name = "Item Autorepair Amount", min = 0, max = Integer.MAX_VALUE)
+    @Entry(name = "Item Autorepair Amount", min = 0, max = Integer.MAX_VALUE, itemDisplay = "randomisfits:zurite_ingot")
     public static int itemAutorepairAmount = 1;
 }
